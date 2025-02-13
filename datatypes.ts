059d598e74d77a988a9ac83f2256cb04b4d130a4
VaminTheegala <94596902+VaminTheegala@users.noreply.github.com>
2025-02-13 15:01:02 +0900

    Update datatypes.ts

--- datatypes.ts
+++ datatypes.ts
@@ -1,2 +1,9 @@
 let str1:string = "First Program in typescript";
 console.log(str1);
+
+let num:number = 1025;
+console.log(num);
+
+let marks:number[] = [42,48,50,38,44,49];
+marks.reduce((mark,sum)=>sum+mark);
+console.log(sum)
