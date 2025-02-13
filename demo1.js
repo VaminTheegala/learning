3cf67b6fa0d1340a99c729af9c0c7073ee6ca6e6
vtheegala <vamshi.theegala@atmecs.com>
2025-02-13 19:15:01 +0530

    even Odd logic

---
+++ demo1.js
@@ -0,0 +1,12 @@
+var evenNumbers = [];
+var oddNumbers = [];
+for (var i = 0; i <= 15; i++) {
+    if (i % 2 == 0) {
+        evenNumbers.push(i);
+        console.log(evenNumbers);
+    }
+    else {
+        oddNumbers.push(i);
+        console.log(oddNumbers);
+    }
+}
---
+++ demo1.ts
@@ -0,0 +1,19 @@
+
+let evenNumbers:number[]=[];
+let oddNumbers:number[]=[];
+for(let i:number=0;i<=15;i++){
+
+   if(i%2==0){
+    evenNumbers.push(i);
+    console.log(evenNumbers);
+   }
+   else{
+    oddNumbers.push(i);
+    console.log(oddNumbers);
+   }
+  
+}
+
+
+
+    
